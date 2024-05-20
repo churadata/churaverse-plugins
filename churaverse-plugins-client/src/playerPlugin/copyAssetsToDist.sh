28ada7858ad4ec60bbcc8b52811622b8101274fa
Takuya Takamine <t.takamine@churadata.okinawa>
2024-05-20 09:08:36 +0000

    アセットcopy用のスクリプトを更新

--- churaverse-plugins-client/src/playerPlugin/copyAssetsToDist.sh
+++ churaverse-plugins-client/src/playerPlugin/copyAssetsToDist.sh
@@ -9,25 +9,26 @@ dest_base="dist"
 find "$src_base" -type d -name 'assets' | while read src_dir; do
     # 対応するコピー先のディレクトリパスを生成
     dest_dir="${src_dir/$src_base/$dest_base}"
+    # コピー元が dest_base ならスキップする処理
+    if [[ "$src_dir" == *"$dest_base"* ]]; then
+        continue
+    fi
     # コピー先のディレクトリがなければ作成
     mkdir -p "$dest_dir"
     # ファイルをコピー先にコピー
     cp -r "$src_dir/"* "$dest_dir"
 done
 
-echo "assetsコピー完了"
-
-#!/bin/bash
-
-# コピー元の基本パス
-src_base="."
-# コピー先の基本パス
-dest_base="dist"
+echo "copied asset files"
 
 # コピー元のパスを検索し、それぞれに対して処理を実行
 find "$src_base" -type f -name '*.scss' | while read src_file; do
     # 対応するコピー先のファイルパスを生成
     dest_file="${src_file/$src_base/$dest_base}"
+    # コピー元が dest_base ならスキップする処理
+    if [[ "$src_dir" == *"$dest_base"* ]]; then
+        continue
+    fi
     # コピー先のディレクトリを取得
     dest_dir=$(dirname "$dest_file")
     # コピー先のディレクトリがなければ作成
