e8867044f5ff69ac29dae03626a6f5c947d3b54d
Takuya Takamine <t.takamine@churadata.okinawa>
2024-07-29 10:57:09 +0000

    camera Video Chat Plugin (client)更新

--- churaverse-plugins-client/src/cameraVideoChatPlugin/ui/videoChatIcon.ts
+++ churaverse-plugins-client/src/cameraVideoChatPlugin/ui/videoChatIcon.ts
@@ -3,8 +3,8 @@ import { ITopBarIconContainer } from '@churaverse/core-ui-plugin-client/interfac
 import { IEventBus, IMainScene } from 'churaverse-engine-client'
 import { TriggerVideoSendingEvent } from '../event/event'
 
-const VIDEO_ON_ICON_PATH = new URL('./assets/video.png', import.meta.url).href
-const VIDEO_OFF_ICON_PATH = new URL('./assets/video_off.png', import.meta.url).href
+import VIDEO_ON_ICON_PATH from './assets/video.png'
+import VIDEO_OFF_ICON_PATH from './assets/video_off.png'
 
 export class VideoChatIcon extends TopBarIconRenderer {
   private readonly iconDivContainer: HTMLDivElement
