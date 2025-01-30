9dc8e8d6b1d77660d678c823520bcc3f2f72c202
e225724 <e225724@eve.u-ryukyu.ac.jp>
2025-01-30 18:23:03 +0900

    レビュー修正

--- churaverse-plugins-server/src/synchroBreakPlugin/synchroBreakPlugin.ts
+++ churaverse-plugins-server/src/synchroBreakPlugin/synchroBreakPlugin.ts
@@ -1,11 +1,7 @@
 import { IMainScene } from 'churaverse-engine-server'
 import { NetworkPluginStore } from '@churaverse/network-plugin-server/store/defNetworkPluginStore'
 import { BaseGamePlugin } from '@churaverse/game-plugin-server/domain/baseGamePlugin'
-import { PriorGameDataEvent } from '@churaverse/game-plugin-server/event/priorGameDataEvent'
-import { PriorGameDataMessage } from '@churaverse/game-plugin-server/message/priorGameDataMessage'
 import { GameStartEvent } from '@churaverse/game-plugin-server/event/gameStartEvent'
-import { GameEndEvent } from '@churaverse/game-plugin-server/event/gameEndEvent'
-import { GameAbortEvent } from '@churaverse/game-plugin-server/event/gameAbortEvent'
 import { SynchroBreakPluginStore } from './store/defSynchroBreakPluginStore'
 import { initSynchroBreakPluginStore, resetSynchroBreakPluginStore } from './store/synchroBreakPluginStoreManager'
 import { SocketController } from './controller/socketController'
@@ -33,20 +29,16 @@ export class SynchroBreakPlugin extends BaseGamePlugin {
   /**
    * ゲームが開始された時に登録されるイベントリスナー
    */
-  private addListenEvent(): void {
-    this.bus.subscribeEvent('gameAbort', this.gameAbortSynchroBreak)
-    this.bus.subscribeEvent('gameEnd', this.gameEndSynchroBreak)
-    this.bus.subscribeEvent('priorGameData', this.priorGameData)
+  protected addListenEvent(): void {
+    super.addListenEvent()
     this.bus.subscribeEvent('timeLimitConfirm', this.timeLimitConfirm)
   }
 
   /**
    * ゲームが終了・中断された時に削除されるイベントリスナー
    */
-  private deleteListenEvent(): void {
-    this.bus.unsubscribeEvent('gameAbort', this.gameAbortSynchroBreak)
-    this.bus.unsubscribeEvent('gameEnd', this.gameEndSynchroBreak)
-    this.bus.unsubscribeEvent('priorGameData', this.priorGameData)
+  protected deleteListenEvent(): void {
+    super.deleteListenEvent()
     this.bus.unsubscribeEvent('timeLimitConfirm', this.timeLimitConfirm)
   }
 
@@ -67,38 +59,12 @@ export class SynchroBreakPlugin extends BaseGamePlugin {
   }
 
   /**
-   * ゲームが中断された時の処理
+   * シンクロブレイク特有の中断・終了時に実行される処理
    */
-  private readonly gameAbortSynchroBreak = (ev: GameAbortEvent): void => {
-    if (ev.gameId !== this.gameId) return
-    this.gameAbort(ev.playerId)
-    this.handleGameTermination()
-  }
-
-  /**
-   * ゲームが終了した時の処理
-   */
-  private readonly gameEndSynchroBreak = (ev: GameEndEvent): void => {
-    if (ev.gameId !== this.gameId) return
-    this.gameEnd()
-    this.handleGameTermination()
-  }
-
-  /**
-   * ゲームの終了・中断時の共通処理
-   */
-  private handleGameTermination(): void {
+  protected handleGameTermination(): void {
     this.deleteListenEvent()
     resetSynchroBreakPluginStore(this.store)
     this.socketController.unregisterMessageListener()
-  }
-
-  /**
-   * プレイヤーが途中参加した際の処理。シンクロブレイクゲームが開始されている場合、メッセージを送信する
-   */
-  private readonly priorGameData = (ev: PriorGameDataEvent): void => {
-    if (!this.isActive) return
-    this.networkPluginStore.messageSender.send(new PriorGameDataMessage({ runningGameId: this.gameId }))
   }
 
   /**
