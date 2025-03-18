a758683738f4338ce60626f7ffc733f70fa6b049
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-03-18 18:44:31 +0900

    server側でも終了、中断のイベントを追加

--- churaverse-plugins-server/src/churarenPlugin/churarenEngine/churarenEngine.ts
+++ churaverse-plugins-server/src/churarenPlugin/churarenEngine/churarenEngine.ts
@@ -1,4 +1,6 @@
 import { BasePlugin, IMainScene } from 'churaverse-engine-server'
+import { GameAbortEvent } from '@churaverse/game-plugin-server/event/gameAbortEvent'
+import { GameEndEvent } from '@churaverse/game-plugin-server/event/gameEndEvent'
 import { GameStartEvent } from '@churaverse/game-plugin-server/event/gameStartEvent'
 import { GameIds } from '@churaverse/game-plugin-server/interface/gameIds'
 import { GamePluginStore } from '@churaverse/game-plugin-server/store/defGamePluginStore'
@@ -13,11 +15,31 @@ export abstract class ChurarenEngine extends BasePlugin<IMainScene> {
     this.bus.subscribeEvent('gameStart', this.gameStart.bind(this))
   }
 
+  protected subscribeGameEvent(): void {
+    this.bus.subscribeEvent('gameEnd', this.gameEnd)
+    this.bus.subscribeEvent('gameAbort', this.gameAbort)
+  }
+
+  protected unsubscribeGameEvent(): void {
+    this.bus.unsubscribeEvent('gameEnd', this.gameEnd)
+    this.bus.unsubscribeEvent('gameAbort', this.gameAbort)
+  }
+
   private gameStart(ev: GameStartEvent): void {
     this.gameId = ev.gameId
     this.gamePluginStore = this.store.of('gamePlugin')
     this.churarenGamePluginStore = this.gamePluginStore.games.get(this.gameId)
     this.handleGameStart()
+  }
+
+  private readonly gameEnd = (ev: GameEndEvent): void => {
+    if (ev.gameId !== this.gameId) return
+    this.handleGameTermination()
+  }
+
+  private readonly gameAbort = (ev: GameAbortEvent): void => {
+    if (ev.gameId !== this.gameId) return
+    this.handleGameTermination()
   }
 
   protected abstract handleGameStart(): void
