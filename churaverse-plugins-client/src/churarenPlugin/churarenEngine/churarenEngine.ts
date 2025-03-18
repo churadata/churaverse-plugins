28f4e2df53e189ccc415f66b703922caba15090a
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-03-18 18:41:26 +0900

    終了、中断のイベントを追加

--- churaverse-plugins-client/src/churarenPlugin/churarenEngine/churarenEngine.ts
+++ churaverse-plugins-client/src/churarenPlugin/churarenEngine/churarenEngine.ts
@@ -1,6 +1,8 @@
 import { BasePlugin, IMainScene } from 'churaverse-engine-client'
 import { GameStartEvent } from '@churaverse/game-plugin-client/event/gameStartEvent'
 import { GameIds } from '@churaverse/game-plugin-client/interface/gameIds'
+import { GameEndEvent } from '@churaverse/game-plugin-client/event/gameEndEvent'
+import { GameAbortEvent } from '@churaverse/game-plugin-client/event/gameAbortEvent'
 
 export abstract class ChurarenEngine extends BasePlugin<IMainScene> {
   public gameId!: GameIds
@@ -8,9 +10,29 @@ export abstract class ChurarenEngine extends BasePlugin<IMainScene> {
     this.bus.subscribeEvent('gameStart', this.gameStart.bind(this))
   }
 
+  protected subscribeGameEvent(): void {
+    this.bus.subscribeEvent('gameEnd', this.gameEnd)
+    this.bus.subscribeEvent('gameAbort', this.gameAbort)
+  }
+
+  protected unsubscribeGameEvent(): void {
+    this.bus.unsubscribeEvent('gameEnd', this.gameEnd)
+    this.bus.unsubscribeEvent('gameAbort', this.gameAbort)
+  }
+
   private gameStart(ev: GameStartEvent): void {
     this.gameId = ev.gameId
     this.handleGameStart()
+  }
+
+  private readonly gameEnd = (ev: GameEndEvent): void => {
+    if (ev.gameId !== this.gameId) return
+    this.handleGameTermination()
+  }
+
+  private readonly gameAbort = (ev: GameAbortEvent): void => {
+    if (ev.gameId !== this.gameId) return
+    this.handleGameTermination()
   }
 
   protected abstract handleGameStart(): void
