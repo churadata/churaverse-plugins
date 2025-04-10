c151de7f70531f5f2075a8504f3eb46cb27e1e85
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-04-10 20:15:31 +0900

    churarenWeaponDamageCauseの実装

--- churaverse-plugins-client/src/churarenPlugin/churarenCorePlugin/index.ts
+++ churaverse-plugins-client/src/churarenPlugin/churarenCorePlugin/index.ts
@@ -1,7 +1,9 @@
-export { ChurarenEnemyDamageCause } from './model/churarenEnemyDamageCause'
-
 export { CHURAREN_CONSTANTS } from './constants/churarenConstants'
 export type { UpdateChurarenUiType, ChurarenUiState, ChurarenGameResult } from './types/uiTypes'
 export { isChurarenGameResult, isChurarenUiState } from './types/uiTypes'
+
+export { ChurarenEnemyDamageCause } from './model/churarenEnemyDamageCause'
+export { ChurarenWeaponDamageCause } from './model/churarenWeaponDamageCause'
+export { type ChurarenWeaponEntity } from './model/churarenWeaponEntity'
 
 export { ChurarenCorePlugin } from './churarenCorePlugin'
