9086dd8d330a4c537441a961f1e3b5fa38bd530c
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-04-14 21:01:12 +0900

    churarenのutilの追加

--- churaverse-plugins-client/src/churarenPlugin/churarenCorePlugin/index.ts
+++ churaverse-plugins-client/src/churarenPlugin/churarenCorePlugin/index.ts
@@ -1,9 +1,16 @@
+// constant
 export { CHURAREN_CONSTANTS } from './constants/churarenConstants'
+
+// types
 export type { UpdateChurarenUiType, ChurarenUiState, ChurarenGameResult } from './types/uiTypes'
 export { isChurarenGameResult, isChurarenUiState } from './types/uiTypes'
 
+// model
 export { ChurarenEnemyDamageCause } from './model/churarenEnemyDamageCause'
 export { ChurarenWeaponDamageCause } from './model/churarenWeaponDamageCause'
 export { type ChurarenWeaponEntity } from './model/churarenWeaponEntity'
+
+// util
+export { isWeaponEntity } from './util/isWeaponEntity'
 
 export { ChurarenCorePlugin } from './churarenCorePlugin'
