c0dee8746e9700ba68f42da4f0e8978d0b592b0e
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-04-30 14:01:48 +0900

    前のコードの削除

--- churaverse-plugins-client/src/gamePlugin/domain/coreGamePlugin.ts
+++ churaverse-plugins-client/src/gamePlugin/domain/coreGamePlugin.ts
@@ -73,7 +73,6 @@ export abstract class CoreGamePlugin extends BaseGamePlugin implements IGameInfo
     this._isActive = this.gameId === ev.gameId
     if (!this.isActive) return
     this._gameOwnerId = ev.playerId
-    this._participantIds = this.store.of('playerPlugin').players.getAllId()
     this.gamePluginStore.gameUiManager.initializeAllUis(this.gameId)
     this.gamePluginStore.gameLogRenderer.gameStartLog(this.gameName, this.gameOwnerId ?? '')
     this.gameInfoStore.games.set(this.gameId, this)
