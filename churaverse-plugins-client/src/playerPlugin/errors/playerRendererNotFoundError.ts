43e0e0eab86e70d2ec336e25dc7682421e97d986
Yuri Kuniyoshi <e235750@ie.u-ryukyu.ac.jp>
2025-06-09 18:53:42 +0900

    修正漏れの修正

--- churaverse-plugins-client/src/playerPlugin/errors/playerRendererNotFoundError.ts
+++ churaverse-plugins-client/src/playerPlugin/errors/playerRendererNotFoundError.ts
@@ -2,6 +2,6 @@ import { PlayerPluginError } from './playerPluginError'
 
 export class PlayerRendererNotFoundError extends PlayerPluginError {
   public constructor() {
-    super(`playerRendererが見つかりませんでした。`)
+    super('playerRendererが見つかりませんでした。')
   }
 }
