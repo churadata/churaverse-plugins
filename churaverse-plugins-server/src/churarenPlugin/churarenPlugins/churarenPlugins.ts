d020c7db435bf018301fa1d313462ad419163de3
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-06-11 16:27:14 +0900

    itemPluginの追加

--- churaverse-plugins-server/src/churarenPlugin/churarenPlugins/churarenPlugins.ts
+++ churaverse-plugins-server/src/churarenPlugin/churarenPlugins/churarenPlugins.ts
@@ -1,4 +1,5 @@
 import { ChurarenCorePlugin } from '@churaverse/churaren-core-plugin-server'
+import { ChurarenItemPlugin } from '@churaverse/churaren-item-plugin-server'
 import '@churaverse/game-plugin-server/store/defGamePluginStore'
 
-export const churarenPlugins = [ChurarenCorePlugin]
+export const churarenPlugins = [ChurarenCorePlugin, ChurarenItemPlugin]
