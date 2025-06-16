97a22f081865fab36b540b76aae7c63049e98b99
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-06-16 15:25:18 +0900

    churarenPluginsへの追加

--- churaverse-plugins-client/src/churarenPlugin/churarenPlugins/churarenPlugins.ts
+++ churaverse-plugins-client/src/churarenPlugin/churarenPlugins/churarenPlugins.ts
@@ -1,6 +1,7 @@
 import { ChurarenCorePlugin } from '@churaverse/churaren-core-plugin-client'
 import { ChurarenItemPlugin } from '@churaverse/churaren-item-plugin-client'
 import { ChurarenPlayerPlugin } from '@churaverse/churaren-player-plugin-client'
+import { ChurarenAlchemyPlugin } from '@churaverse/churaren-alchemy-plugin-client'
 import '@churaverse/churaren-core-plugin-client/ui/defChurarenUi'
 
-export const churarenPlugins = [ChurarenCorePlugin, ChurarenItemPlugin, ChurarenPlayerPlugin]
+export const churarenPlugins = [ChurarenCorePlugin, ChurarenItemPlugin, ChurarenPlayerPlugin, ChurarenAlchemyPlugin]
