718348cacb39266aa9bc8c53f31c21c17626a1ad
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-06-26 20:03:18 +0900

    churarenPluginsへalchemyItems追加

--- churaverse-plugins-client/src/churarenPlugin/churarenPlugins/churarenPlugins.ts
+++ churaverse-plugins-client/src/churarenPlugin/churarenPlugins/churarenPlugins.ts
@@ -1,7 +1,14 @@
+import { alchemyItems } from '@churaverse/churaren-alchemy-items-client'
 import { ChurarenCorePlugin } from '@churaverse/churaren-core-plugin-client'
 import { ChurarenItemPlugin } from '@churaverse/churaren-item-plugin-client'
 import { ChurarenPlayerPlugin } from '@churaverse/churaren-player-plugin-client'
 import { ChurarenAlchemyPlugin } from '@churaverse/churaren-alchemy-plugin-client'
 import '@churaverse/churaren-core-plugin-client/ui/defChurarenUi'
 
-export const churarenPlugins = [ChurarenCorePlugin, ChurarenItemPlugin, ChurarenPlayerPlugin, ChurarenAlchemyPlugin]
+export const churarenPlugins: any[] = [
+  ChurarenCorePlugin,
+  ChurarenItemPlugin,
+  ChurarenPlayerPlugin,
+  ChurarenAlchemyPlugin,
+  ...alchemyItems,
+]
