bad3c68f1b578ca8d011226887b9f369596fed04
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-07-02 17:01:10 +0900

    中断した際にアイテムの生成も中断できるよう実装

--- churaverse-plugins-server/src/churarenPlugin/churarenItemPlugin/churarenItemPlugin.ts
+++ churaverse-plugins-server/src/churarenPlugin/churarenItemPlugin/churarenItemPlugin.ts
@@ -23,6 +23,7 @@ export class ChurarenItemPlugin extends BaseGamePlugin {
   private networkPluginStore!: NetworkPluginStore<IMainScene>
   private churarenGameInfo?: IGameInfo
   private socketController!: SocketController
+  private itemGenerateIntervalId?: NodeJS.Timeout
 
   public listenEvent(): void {
     super.listenEvent()
@@ -70,6 +71,7 @@ export class ChurarenItemPlugin extends BaseGamePlugin {
   }
 
   protected handleGameTermination(): void {
+    this.clearItemGenerateInterval()
     this.itemPluginStore.items.clear()
   }
 
@@ -85,7 +87,7 @@ export class ChurarenItemPlugin extends BaseGamePlugin {
           this.networkPluginStore.messageSender.send(itemSpawnMessage)
         }
       )
-      setTimeout(generateItemsLoop, ITEM_GENERATE_INTERVAL_MS)
+      this.itemGenerateIntervalId = setTimeout(generateItemsLoop, ITEM_GENERATE_INTERVAL_MS)
     }
     generateItemsLoop()
   }
@@ -103,8 +105,16 @@ export class ChurarenItemPlugin extends BaseGamePlugin {
   }
 
   private readonly removeAllItems = (): void => {
+    this.clearItemGenerateInterval()
     const itemIds = this.itemPluginStore.items.getAllId()
     this.networkPluginStore.messageSender.send(new ChurarenItemDespawnMessage({ itemIds }))
     this.itemPluginStore.items.clear()
+  }
+
+  private clearItemGenerateInterval(): void {
+    if (this.itemGenerateIntervalId !== undefined) {
+      clearTimeout(this.itemGenerateIntervalId)
+      this.itemGenerateIntervalId = undefined
+    }
   }
 }
