b0cb1e828ec2ab85cf12ff2c7c590d9ed1031240
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-07-09 16:49:59 +0900

    回復アイテムのメッセージの作成

--- churaverse-plugins-client/src/churarenPlugin/churarenAlchemyItems/healingPotionPlugin/healingPotionPlugin.ts
+++ churaverse-plugins-client/src/churarenPlugin/churarenAlchemyItems/healingPotionPlugin/healingPotionPlugin.ts
@@ -2,19 +2,28 @@ import { BaseAlchemyItemPlugin } from '@churaverse/churaren-alchemy-plugin-clien
 import { healignPotion, HEALING_POTION_ITEM, HealingPotion } from './domain/healingPotion'
 import { UseAlchemyItemEvent } from '@churaverse/churaren-alchemy-plugin-client/event/useAlchemyItemEvent'
 import { initHealingPotionPluginStore, resetHealingPotionPluginStore } from './store/initHealingPotionPluginStore'
-import { PlayerHealMessage } from '@churaverse/churaren-player-plugin-client/message/playerHealMessage'
 import { HealingPotionPluginStore } from './store/defHealingPotionPluginStore'
 import { ClearAlchemyItemBoxEvent } from '@churaverse/churaren-alchemy-plugin-client/event/clearAlchemyItemBox'
 import '@churaverse/network-plugin-client/store/defNetworkPluginStore'
 import '@churaverse/player-plugin-client/store/defPlayerPluginStore'
+import { UseHealingPotionMessage } from './message/useHealingPotionMessage'
+import { SocketController } from './controller/socketController'
 
 export class HealingPotionPlugin extends BaseAlchemyItemPlugin {
   private healingPotionPluginStore!: HealingPotionPluginStore
+  private socketController?: SocketController
   protected alchemyItemKind = healignPotion
   protected alchemyItem = HEALING_POTION_ITEM
 
   public listenEvent(): void {
     super.listenEvent()
+
+    this.socketController = new SocketController(this.bus, this.store)
+    this.bus.subscribeEvent('registerMessage', this.socketController.registerMessage.bind(this.socketController))
+    this.bus.subscribeEvent(
+      'registerMessageListener',
+      this.socketController.setupMessageListenerRegister.bind(this.socketController)
+    )
   }
 
   protected subscribeGameEvent(): void {
@@ -30,9 +39,11 @@ export class HealingPotionPlugin extends BaseAlchemyItemPlugin {
   protected handleGameStart(): void {
     initHealingPotionPluginStore(this.store)
     this.healingPotionPluginStore = this.store.of('churarenHealingPotionPlugin')
+    this.socketController?.registerMessageListener()
   }
 
   protected handleGameTermination(): void {
+    this.socketController?.unregisterMessageListener()
     resetHealingPotionPluginStore(this.store)
   }
 
@@ -52,7 +63,7 @@ export class HealingPotionPlugin extends BaseAlchemyItemPlugin {
     this.healingPotionPluginStore.healingPotions.set(healingPotion.healingPotionId, healingPotion)
 
     if (healingPotion.ownerId === this.store.of('playerPlugin').ownPlayerId) {
-      const healingPotionMessage = new PlayerHealMessage({
+      const healingPotionMessage = new UseHealingPotionMessage({
         playerId: healingPotion.ownerId,
         healAmount: healingPotion.heal,
       })
