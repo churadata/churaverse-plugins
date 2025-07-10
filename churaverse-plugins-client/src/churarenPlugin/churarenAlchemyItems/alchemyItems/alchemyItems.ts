730128153059b1af3be6aa88e2e90f3c2e9b39c1
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-07-10 20:10:12 +0900

    alchemyItemsへの追加

--- churaverse-plugins-client/src/churarenPlugin/churarenAlchemyItems/alchemyItems/alchemyItems.ts
+++ churaverse-plugins-client/src/churarenPlugin/churarenAlchemyItems/alchemyItems/alchemyItems.ts
@@ -1,3 +1,4 @@
 import { HealingPotionPlugin } from '@churaverse/churaren-healing-potion-plugin-client'
+import { RevivalItemPlugin } from '@churaverse/churaren-revival-item-plugin-client'
 
-export const alchemyItems = [HealingPotionPlugin]
+export const alchemyItems = [HealingPotionPlugin, RevivalItemPlugin]
