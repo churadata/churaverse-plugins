80e2aa979f8d7ca7d8d78c9c5798cb9bb749e614
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-08-04 20:19:17 +0900

    TODOコメントの実装とdependenciesへの追記

--- churaverse-plugins-client/src/churarenPlugin/churarenAlchemyItems/tornadoPlugin/domain/tornado.ts
+++ churaverse-plugins-client/src/churarenPlugin/churarenAlchemyItems/tornadoPlugin/domain/tornado.ts
@@ -1,8 +1,9 @@
 import { AlchemyItem } from '@churaverse/churaren-alchemy-plugin-client/domain/alchemyItem'
 import { AlchemyItemKind } from '@churaverse/churaren-alchemy-plugin-client/domain/alchemyItemKind'
 import { IAlchemyItem } from '@churaverse/churaren-alchemy-plugin-client/domain/IAlchemyItem'
-import { Direction, Position, WeaponEntity } from 'churaverse-engine-client'
+import { Direction, Position } from 'churaverse-engine-client'
 import tornadoImage from '../assets/tornado.png'
+import { ChurarenWeaponEntity } from '@churaverse/churaren-core-plugin-client'
 
 export const TORNADO_WALK_LIMIT_GRIDS = 5
 export const TORNADO_WALK_LIMIT_MS = 500
@@ -15,21 +16,17 @@ export const TORNADO_ITEM: IAlchemyItem = {
 /**
  * Tornadoクラスの定義
  */
-// TODO: CV-706マージ後に`ChurarenWeaponEntity`をimplementsする
-export class Tornado extends AlchemyItem implements WeaponEntity {
+export class Tornado extends AlchemyItem implements ChurarenWeaponEntity {
   public readonly power = 70
   public isDead = false
   public tornadoId: string
   public spawnTime: number
-  // TODO: `ChurarenWeaponEntity`をimplementsした際に、ownerIdを`churarenWeaponOwnerId`に変更し、idは`tornadoId`に統合される
-  public readonly id: string
-  public ownerId: string
+  public churarenWeaponOwnerId: string
 
   public constructor(tornadoId: string, ownerId: string, position: Position, direction: Direction, spawnTime: number) {
     super(tornadoId, TORNADO_ITEM.kind)
-    this.id = tornadoId
     this.tornadoId = tornadoId
-    this.ownerId = ownerId
+    this.churarenWeaponOwnerId = ownerId
     this.position = position
     this.spawnTime = spawnTime
     this.direction = direction
