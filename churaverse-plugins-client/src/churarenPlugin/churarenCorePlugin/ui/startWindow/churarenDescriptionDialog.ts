77abfde3bd35f3e6552623a067b466cf2219374f
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-08-27 18:44:15 +0900

    　ちゅられんdialogの命名修正

--- churaverse-plugins-client/src/churarenPlugin/churarenCorePlugin/ui/startWindow/churarenDescriptionDialog.ts
+++ churaverse-plugins-client/src/churarenPlugin/churarenCorePlugin/ui/startWindow/churarenDescriptionDialog.ts
@@ -2,7 +2,7 @@ import { IMainScene, Store } from 'churaverse-engine-client'
 import { GameDescriptionDialog } from '@churaverse/game-plugin-client/ui/gameDescriptionDialog/gameDescriptionDialog'
 import { CHURAREN_CONSTANTS } from '../../constants/churarenConstants'
 
-export class PopupChurarenRule extends GameDescriptionDialog {
+export class ChurarenDescriptionDialog extends GameDescriptionDialog {
   public constructor(store: Store<IMainScene>) {
     super(store, CHURAREN_CONSTANTS.GAME_ID, CHURAREN_CONSTANTS.GAME_NAME)
   }
--- churaverse-plugins-client/src/churarenPlugin/churarenCorePlugin/ui/startWindow/setupChurarenDialog.ts
+++ churaverse-plugins-client/src/churarenPlugin/churarenCorePlugin/ui/startWindow/setupChurarenDialog.ts
@@ -1,13 +1,13 @@
 import { IMainScene, Store, DomManager } from 'churaverse-engine-client'
 import { RuleExplanationWindowComponent } from './component/RuleExplanationWindowComponent'
-import { PopupChurarenRule } from './popupChurarenRule'
 import { CHURAREN_CONSTANTS } from '../../constants/churarenConstants'
+import { ChurarenDescriptionDialog } from './churarenDescriptionDialog'
 
 /**
  * ゲームリストにちゅられんを追加する関数
  */
 export function setupChurarenDialog(store: Store<IMainScene>): void {
-  const popupChurarenRule = new PopupChurarenRule(store)
+  const popupChurarenRule = new ChurarenDescriptionDialog(store)
   const explanation = DomManager.addJsxDom(RuleExplanationWindowComponent())
   popupChurarenRule.createGameDescription(explanation)
   store.of('gamePlugin').gameDescriptionDialogManager.add(CHURAREN_CONSTANTS.GAME_ID, popupChurarenRule)
