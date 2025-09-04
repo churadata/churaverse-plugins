d822b188191cb569e44d430ba49d413cc2ff3d14
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-09-04 16:30:02 +0900

    型を修正

--- churaverse-plugins-server/src/churarenPlugin/churarenAlchemyItems/alchemyItemPlugins/alchemyItemPlugins.ts
+++ churaverse-plugins-server/src/churarenPlugin/churarenAlchemyItems/alchemyItemPlugins/alchemyItemPlugins.ts
@@ -1,3 +1,3 @@
-import { BaseAlchemyItemPlugin } from '@churaverse/churaren-alchemy-plugin-server/domain/baseAlchemyItemPlugin'
+import { BaseGamePlugin } from '@churaverse/game-plugin-server/domain/baseGamePlugin'
 
-export const alchemyItemPlugins: Array<typeof BaseAlchemyItemPlugin> = []
+export const alchemyItemPlugins: Array<typeof BaseGamePlugin> = []
