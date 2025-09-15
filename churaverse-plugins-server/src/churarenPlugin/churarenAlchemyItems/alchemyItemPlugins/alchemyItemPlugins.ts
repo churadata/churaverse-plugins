c0d6416e92b855d3e3fc6b39923f6263a160b0cd
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-09-15 19:26:22 +0900

    alchemyItemsへの追加

--- churaverse-plugins-server/src/churarenPlugin/churarenAlchemyItems/alchemyItemPlugins/alchemyItemPlugins.ts
+++ churaverse-plugins-server/src/churarenPlugin/churarenAlchemyItems/alchemyItemPlugins/alchemyItemPlugins.ts
@@ -1,3 +1,4 @@
 import { BaseGamePlugin } from '@churaverse/game-plugin-server/domain/baseGamePlugin'
+import { HealingPotionPlugin } from '@churaverse/churaren-healing-potion-plugin-server'
 
 export const alchemyItemPlugins: Array<typeof BaseGamePlugin> = [HealingPotionPlugin]
