da1061e697cea920445067d7de2034efc1c3668b
Kaisei <e225721@ie.u-ryukyu.ac.jp>
2025-09-30 18:30:47 +0900

    コンフリクト解消

--- churaverse-plugins-client/src/churarenPlugin/churarenPlugins/churarenPlugins.ts
+++ churaverse-plugins-client/src/churarenPlugin/churarenPlugins/churarenPlugins.ts
@@ -2,13 +2,18 @@ import { ChurarenBossPlugin } from '@churaverse/churaren-boss-plugin-client'
 import { ChurarenCorePlugin } from '@churaverse/churaren-core-plugin-client'
 import { ChurarenItemPlugin } from '@churaverse/churaren-item-plugin-client'
 import { ChurarenPlayerPlugin } from '@churaverse/churaren-player-plugin-client'
+import { ChurarenAlchemyPlugin } from '@churaverse/churaren-alchemy-plugin-client'
 import '@churaverse/churaren-core-plugin-client/ui/defChurarenUi'
 import { ChurarenBossAttackPlugin } from '@churaverse/churaren-boss-attack-plugin-client'
+import { alchemyItemPlugins } from '@churaverse/churaren-alchemy-item-plugins-client'
+import { BaseGamePlugin } from '@churaverse/game-plugin-client/domain/baseGamePlugin'
 
-export const churarenPlugins = [
+export const churarenPlugins: Array<typeof BaseGamePlugin> = [
   ChurarenCorePlugin,
   ChurarenBossPlugin,
   ChurarenItemPlugin,
   ChurarenPlayerPlugin,
   ChurarenBossAttackPlugin,
+  ChurarenAlchemyPlugin,
+  ...alchemyItemPlugins,
 ]
--- churaverse-plugins-server/src/churarenPlugin/churarenPlugins/churarenPlugins.ts
+++ churaverse-plugins-server/src/churarenPlugin/churarenPlugins/churarenPlugins.ts
@@ -1,15 +1,19 @@
 import { ChurarenBossPlugin } from '@churaverse/churaren-boss-plugin-server'
+import { ChurarenAlchemyPlugin } from '@churaverse/churaren-alchemy-plugin-server'
 import { ChurarenCorePlugin } from '@churaverse/churaren-core-plugin-server'
 import { ChurarenItemPlugin } from '@churaverse/churaren-item-plugin-server'
 import { ChurarenPlayerPlugin } from '@churaverse/churaren-player-plugin-server'
 import { ChurarenBossAttackPlugin } from '@churaverse/churaren-boss-attack-plugin-server'
+import { BaseGamePlugin } from '@churaverse/game-plugin-server/domain/baseGamePlugin'
 
 import '@churaverse/game-plugin-server/store/defGamePluginStore'
 
-export const churarenPlugins = [
+export const churarenPlugins: Array<typeof BaseGamePlugin> = [
   ChurarenCorePlugin,
   ChurarenBossPlugin,
   ChurarenItemPlugin,
   ChurarenPlayerPlugin,
   ChurarenBossAttackPlugin,
+  ChurarenAlchemyPlugin,
+  ...alchemyItemPlugins,
 ]
