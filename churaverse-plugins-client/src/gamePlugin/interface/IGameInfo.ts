9f66ef4ec695832f200c204fcb8795b4a0ba9916
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-10-23 20:10:43 +0900

    参加しているかのフラグを保持

--- churaverse-plugins-client/src/gamePlugin/interface/IGameInfo.ts
+++ churaverse-plugins-client/src/gamePlugin/interface/IGameInfo.ts
@@ -6,6 +6,7 @@ import { GameIds } from './gameIds'
 export interface IGameInfo {
   readonly gameId: GameIds
   readonly isActive: boolean
+  readonly isJoinGame: boolean
   readonly gameOwnerId: string | undefined
   readonly participantIds: string[]
   readonly isOwnPlayerMidwayParticipant: boolean
