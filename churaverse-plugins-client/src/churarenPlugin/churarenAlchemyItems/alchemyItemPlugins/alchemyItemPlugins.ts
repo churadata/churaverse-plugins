ee69a87f70844a6e2129c822c5889d78ee13dde3
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-11-10 19:23:52 +0900

    プラグインの追加

--- churaverse-plugins-client/src/churarenPlugin/churarenAlchemyItems/alchemyItemPlugins/alchemyItemPlugins.ts
+++ churaverse-plugins-client/src/churarenPlugin/churarenAlchemyItems/alchemyItemPlugins/alchemyItemPlugins.ts
@@ -1,3 +1,4 @@
 import { BaseAlchemyItemPlugin } from '@churaverse/churaren-alchemy-plugin-client/domain/baseAlchemyItemPlugin'
+import { FlamePillarPlugin } from '@churaverse/churaren-flame-pillar-plugin-client'
 
-export const alchemyItemPlugins: Array<typeof BaseAlchemyItemPlugin> = []
+export const alchemyItemPlugins: Array<typeof BaseAlchemyItemPlugin> = [FlamePillarPlugin]
