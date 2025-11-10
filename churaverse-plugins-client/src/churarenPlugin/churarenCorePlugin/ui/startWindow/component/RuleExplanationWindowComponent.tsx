110bc1b2285c40ec2dc8b28d2eb4832475cdd1b3
Kaisei <e225721@ie.u-ryukyu.ac.jp>
2025-11-10 11:12:14 +0900

    見出しの追加と境目追加

--- churaverse-plugins-client/src/churarenPlugin/churarenCorePlugin/ui/startWindow/component/RuleExplanationWindowComponent.tsx
+++ churaverse-plugins-client/src/churarenPlugin/churarenCorePlugin/ui/startWindow/component/RuleExplanationWindowComponent.tsx
@@ -14,13 +14,11 @@ export const RuleExplanationWindowComponent: JSXFunc = () => {
     <div className={style.container} id={POPUP_GAME_START_WINDOW_BUTTON_ID}>
       <h2 className={style.title}>ちゅられん</h2>
       <p className={style.description}>みんなで協力し、制限時間内に強大なモンスターに立ち向かう協力型バトルゲーム！</p>
-
       <div className={style.section}>
         <img src={playingImage} alt="プレイ中の様子を写した画像" className={style.headerImage} />
       </div>
 
       <h2 className={style.churarenRule}>⚔️ちゅられんルール説明⚔️</h2>
-
       <ol className={style.sectionList}>
         <li className={style.stepItem}>
           <p className={style.stepTitle}>アイテムを集めよう！</p>
@@ -40,31 +38,27 @@ export const RuleExplanationWindowComponent: JSXFunc = () => {
           <img src={bossImage} alt="錬金した攻撃とボスの写った画像" className={style.image} />
         </li>
       </ol>
-
+      <hr className={style.divider} />
+      <h3 className={style.churarenMode}>👻ゴーストモードについて👻</h3>
       <div className={style.section}>
-        <h3>ゴーストモード</h3>
         <p>
           ゲーム途中でボスの攻撃によってHPがゼロになってしまったプレイヤーは、ゴーストモード(※3)へ移行し、最後まで観戦することができます。
         </p>
         <img src={ghostImage} alt="ゴーストモードプレイヤーの写った画像" className={style.image} />
       </div>
 
+      <h2 className={style.churarenJudgeHeader}>🚩勝敗判定🚩</h2>
       <div className={style.section}>
-        <h4>勝利条件</h4>
-        <ul style={{ listStylePosition: 'inside', paddingLeft: '0.5em' }}>
-          <li>制限時間3分以内にボスモンスターのHPをゼロにする</li>
-        </ul>
+        <h4 className={`${style.churarenJudge} ${style.win}`}>勝利条件</h4>
+        <li>制限時間3分以内にボスモンスターのHPをゼロにする</li>
 
-        <h4>敗北条件</h4>
-        <ul style={{ listStylePosition: 'inside', paddingLeft: '0.5em' }}>
-          <li>制限時間経過後にボスのHPが残っている</li>
-          <li>参加プレイヤー全員がゴーストモードに移行した</li>
-        </ul>
+        <h4 className={`${style.churarenJudge} ${style.lose}`}>敗北条件</h4>
+        <li>制限時間経過後にボスのHPが残っている</li>
+        <li>参加プレイヤー全員がゴーストモードに移行した</li>
       </div>
-
       <div className={style.section}>
         <h5>補足</h5>
-        <ul style={{ listStyleType: 'none', paddingLeft: 0, margin: 0 }}>
+        <ul>
           <li>(※1) 参加者が多いほど、それに応じてボスのHPも増加します</li>
           <li>(※2) 錬金に使うアイテムの組み合わせによって、攻撃の種類が変わります</li>
           <li>(※3) ゴーストモードでは攻撃・錬金などのアクションは無効になります</li>
