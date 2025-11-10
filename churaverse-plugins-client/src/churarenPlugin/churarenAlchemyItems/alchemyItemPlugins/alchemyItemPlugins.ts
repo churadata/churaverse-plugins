78bc14179ba4275977a51af605cb30ecfedf4f90
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2025-11-10 20:31:31 +0900

    プラグインの追加

--- churaverse-plugins-client/src/churarenPlugin/churarenAlchemyItems/alchemyItemPlugins/alchemyItemPlugins.ts
+++ churaverse-plugins-client/src/churarenPlugin/churarenAlchemyItems/alchemyItemPlugins/alchemyItemPlugins.ts
@@ -1,3 +1,4 @@
 import { BaseAlchemyItemPlugin } from '@churaverse/churaren-alchemy-plugin-client/domain/baseAlchemyItemPlugin'
+import { BlackHolePlugin } from '../blackHolePlugin/blackHolePlugin'
 
-export const alchemyItemPlugins: Array<typeof BaseAlchemyItemPlugin> = []
+export const alchemyItemPlugins: Array<typeof BaseAlchemyItemPlugin> = [BlackHolePlugin]
