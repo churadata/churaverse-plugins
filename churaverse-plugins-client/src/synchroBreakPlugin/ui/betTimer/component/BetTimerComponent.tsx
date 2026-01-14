8fdb914d75ad9c4fba6702398ee6ccd0df5b41eb
Yuri Kuniyoshi <e235750@ie.u-ryukyu.ac.jp>
2026-01-14 14:44:28 +0900

    不要な記述を削除

--- churaverse-plugins-client/src/synchroBreakPlugin/ui/betTimer/component/BetTimerComponent.tsx
+++ churaverse-plugins-client/src/synchroBreakPlugin/ui/betTimer/component/BetTimerComponent.tsx
@@ -4,7 +4,7 @@ import { BET_TIMER_CONTAINER_ID, BET_TIMER_ID } from '../betTimer'
 
 export const BetTimerComponent: JSXFunc = () => {
   return (
-    <div className={`${style.betTimerContainer}`} id={BET_TIMER_CONTAINER_ID}>
+    <div className={style.betTimerContainer} id={BET_TIMER_CONTAINER_ID}>
       <div className={style.betTimer} id={BET_TIMER_ID}></div>
     </div>
   )
