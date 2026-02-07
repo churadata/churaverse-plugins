b56c3c8a39bf0a355564a621716ab847e3f927b9
Yuri Kuniyoshi <e235750@ie.u-ryukyu.ac.jp>
2026-02-07 14:49:43 +0900

    descriptionTextとDOMの二重管理を解消

--- churaverse-plugins-client/src/synchroBreakPlugin/ui/descriptionWindow/descriptionWindow.ts
+++ churaverse-plugins-client/src/synchroBreakPlugin/ui/descriptionWindow/descriptionWindow.ts
@@ -8,10 +8,21 @@ export const SYNCHRO_BREAK_DESCRIPTION_TEXT_ID = 'synchro-break-description-text
 export class DescriptionWindow implements IDescriptionWindow {
   public element!: HTMLElement
   public visible: boolean = false
-  private descriptionText: string = ''
   private descriptionTextElement?: HTMLElement
   private gameName: string = 'ゲーム'
   private gameOwnerName: string = 'ゲームオーナー'
+
+  private get descriptionText(): string {
+    this.descriptionTextElement = this.descriptionTextElement ?? DomManager.getElementById(SYNCHRO_BREAK_DESCRIPTION_TEXT_ID)
+    return this.descriptionTextElement.innerHTML ?? ''
+  }
+
+  private set descriptionText(text: string) {
+    this.descriptionTextElement = this.descriptionTextElement ?? DomManager.getElementById(SYNCHRO_BREAK_DESCRIPTION_TEXT_ID)
+    if (this.descriptionTextElement !== undefined) {
+      this.descriptionTextElement.innerHTML = text
+    }
+  }
 
   public initialize(): void {
     this.element = DomManager.addJsxDom(DescriptionWindowComponent({ description: this.descriptionText }))
@@ -36,9 +47,7 @@ export class DescriptionWindow implements IDescriptionWindow {
    * ゲーム開始時主催者サイドの文章更新処理
    */
   public displayGameStartForOwner(): void {
-    this.setDescriptionText(
-      `${this.gameName}を開始しました。<br>あなたはゲームの管理者です。 <br>ターン数(1~10)を選択してください。`
-    )
+    this.descriptionText = `${this.gameName}を開始しました。<br>あなたはゲームの管理者です。 <br>ターン数(1~10)を選択してください。`
     this.open()
   }
 
@@ -46,7 +55,7 @@ export class DescriptionWindow implements IDescriptionWindow {
    * ゲーム開始時参加者サイドの文章更新処理
    */
   public displayGameStartForGuest(): void {
-    this.setDescriptionText(`${this.gameName}が開始されました！<br>${this.gameOwnerName}さんがターンを入力中です。`)
+    this.descriptionText = `${this.gameName}が開始されました！<br>${this.gameOwnerName}さんがターンを入力中です。`
     this.open()
   }
 
@@ -55,7 +64,7 @@ export class DescriptionWindow implements IDescriptionWindow {
    * @param turn 選択されたターン数
    */
   public displayTurnSelectionForOwner(turn: number): void {
-    this.setDescriptionText(`${turn}ターン選択しました。<br>制限時間(3~15)を選択してください。`)
+    this.descriptionText = `${turn}ターン選択しました。<br>制限時間(3~15)を選択してください。`
   }
 
   /**
@@ -63,7 +72,7 @@ export class DescriptionWindow implements IDescriptionWindow {
    * @param turn 選択されたターン数
    */
   public displayTurnSelectionForGuest(turn: number): void {
-    this.setDescriptionText(`${turn}ターン選択しました。<br>${this.gameOwnerName}さんが制限時間を入力中です。`)
+    this.descriptionText = `${turn}ターン選択しました。<br>${this.gameOwnerName}さんが制限時間を入力中です。`
   }
 
   /**
@@ -71,9 +80,7 @@ export class DescriptionWindow implements IDescriptionWindow {
    * @param timeLimit 選択された制限時間
    */
   public displayTimeLimitSelectionForOwner(timeLimit: string, ownCoins: number): void {
-    this.setDescriptionText(
-      `制限時間を${timeLimit}秒選択しました。<br>ベットコインを入力してください。<br>所持コイン数:${ownCoins}`
-    )
+    this.descriptionText = `制限時間を${timeLimit}秒選択しました。<br>ベットコインを入力してください。<br>所持コイン数:${ownCoins}`
   }
 
   /**
@@ -81,9 +88,7 @@ export class DescriptionWindow implements IDescriptionWindow {
    * @param timeLimit 選択された制限時間
    */
   public displayTimeLimitSelectionForGuest(timeLimit: string, ownCoins: number): void {
-    this.setDescriptionText(
-      `制限時間が${timeLimit}秒選択されました。<br>ベットコインを入力してください。<br>所持コイン数:${ownCoins}`
-    )
+    this.descriptionText = `制限時間が${timeLimit}秒選択されました。<br>ベットコインを入力してください。<br>所持コイン数:${ownCoins}`
   }
 
   /**
@@ -91,7 +96,7 @@ export class DescriptionWindow implements IDescriptionWindow {
    * @param betCoins 選択されたベットコイン数
    */
   public displayBetCoinSelection(betCoins: number): void {
-    this.setDescriptionText(`ベットコインを${betCoins}枚選択しました。<br>相手のベットコインを待っています。`)
+    this.descriptionText = `ベットコインを${betCoins}枚選択しました。<br>相手のベットコインを待っています。`
   }
 
   /**
@@ -99,14 +104,14 @@ export class DescriptionWindow implements IDescriptionWindow {
    * @param countdown ゲーム開始までのカウントダウン
    */
   public displayGameStartCountdown(countdown: number): void {
-    this.setDescriptionText(`ゲーム開始まで<br>${countdown}秒`)
+    this.descriptionText = `ゲーム開始まで<br>${countdown}秒`
   }
 
   /**
    * シンクロブレイク開始の文章更新処理
    */
   public displaySynchroBreakStart(): void {
-    this.setDescriptionText(`${this.gameName}開始！！！<br>制限時間以内にボタンを押してください！`)
+    this.descriptionText = `${this.gameName}開始！！！<br>制限時間以内にボタンを押してください！`
   }
 
   /**
@@ -118,14 +123,14 @@ export class DescriptionWindow implements IDescriptionWindow {
       descriptionText.splice(1, 0, nyokkiActionMessage)
     }
 
-    this.setDescriptionText(descriptionText.join('<br>'))
+    this.descriptionText = descriptionText.join('<br>')
   }
 
   /**
    * シンクロブレイク終了の文章更新処理
    */
   public displaySynchroBreakEnd(): void {
-    this.setDescriptionText(`${this.gameName}終了！！！`)
+    this.descriptionText = `${this.gameName}終了！！！`
   }
 
   /**
@@ -133,9 +138,7 @@ export class DescriptionWindow implements IDescriptionWindow {
    * @param turnNumber 残りターン数
    */
   public displayTurnStart(turnNumber: number, ownCoins: number): void {
-    this.setDescriptionText(
-      `残り${turnNumber}ターンです。<br>ベットコインを入力してください。<br>所持コイン数:${ownCoins}`
-    )
+    this.descriptionText = `残り${turnNumber}ターンです。<br>ベットコインを入力してください。<br>所持コイン数:${ownCoins}`
   }
 
   /**
@@ -145,30 +148,17 @@ export class DescriptionWindow implements IDescriptionWindow {
   public displayNyokkiAction(text: string): void {
     const descriptionText = this.descriptionText.split('<br>')
     descriptionText.splice(1, 0, text)
-    this.setDescriptionText(descriptionText.join('<br>'))
+    this.descriptionText = descriptionText.join('<br>')
   }
 
   /**
    * 結果ウィンドウ表示時のメッセージ表示
    */
   public displayResultMessage(): void {
-    this.setDescriptionText(
-      '〜最終ランキング〜</br>お疲れ様でした。</br>閉じるボタンを押すと通常のちゅらバースに戻ります。'
-    )
+    this.descriptionText = '〜最終ランキング〜</br>お疲れ様でした。</br>閉じるボタンを押すと通常のちゅらバースに戻ります。'
   }
 
   private open(): void {
     this.element.style.display = 'flex'
-  }
-
-  /**
-   * 説明ウィンドウの文章を更新する
-   * @param text 更新する文章
-   */
-  private setDescriptionText(text: string): void {
-    this.descriptionTextElement = this.descriptionTextElement ?? DomManager.getElementById(SYNCHRO_BREAK_DESCRIPTION_TEXT_ID)
-    if (this.descriptionTextElement === null) return
-    this.descriptionTextElement.innerHTML = text
-    this.descriptionText = text
   }
 }
