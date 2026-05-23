0ee4fa5b6154ae7878ed7170198b124cd63aa962
TsuhaShuto-hugkun <s.tsuha134@hugkun.com>
2026-05-23 16:29:51 +0900

    サーバー側のロジックをクライアント側と同様に変更

--- churaverse-plugins-server/src/churarenPlugin/churarenAlchemyItems/waterRingPlugin/repository/waterRingRepository.ts
+++ churaverse-plugins-server/src/churarenPlugin/churarenAlchemyItems/waterRingPlugin/repository/waterRingRepository.ts
@@ -4,19 +4,13 @@ import { IWaterRingRepository } from '../domain/IWaterRingRepository'
 
 export class WaterRingRepository extends CollidableEntityRepository<WaterRing> implements IWaterRingRepository {
   private readonly waterRings = new Map<string, WaterRing>()
-  private readonly ownerIdIndex = new Map<string, string>()
 
   public set(id: string, entity: WaterRing): void {
     super.set(id, entity)
     this.waterRings.set(id, entity)
-    this.ownerIdIndex.set(entity.churarenWeaponOwnerId, id)
   }
 
   public delete(id: string): void {
-    const waterRing = this.waterRings.get(id)
-    if (waterRing !== undefined) {
-      this.ownerIdIndex.delete(waterRing.churarenWeaponOwnerId)
-    }
     super.delete(id)
     this.waterRings.delete(id)
   }
@@ -30,7 +24,6 @@ export class WaterRingRepository extends CollidableEntityRepository<WaterRing> i
   }
 
   public getByOwnerId(ownerId: string): WaterRing | undefined {
-    const waterRingId = this.ownerIdIndex.get(ownerId)
-    return waterRingId !== undefined ? this.waterRings.get(waterRingId) : undefined
+    return Array.from(this.waterRings.values()).find((waterRing) => waterRing.churarenWeaponOwnerId === ownerId)
   }
 }
